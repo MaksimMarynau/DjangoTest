33777ad2cddd3c6357ac0c54af4cd275663995da
MaksimMarynau <oliverknightbel@gmail.com>
2020-06-05 15:42:02 +0200

    first commit

---
+++ myfirst/myfirst/apps/articles/models.py
@@ -0,0 +1,23 @@
+import datetime
+from django.db import models
+
+from django.utils import timezone
+
+class Article(models.Model):
+    article_title = models.CharField('Name of publication', max_length=200)
+    article_text = models.TextField('Text of publication')
+    pub_date = models.DateTimeField('Date of publication')
+
+    def __str__(self):
+        return self.article_title
+
+    def was_published_recently(self):
+        return self.pub_date >= (timezone.now() - datetime.timedelta(days=7))
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    author_name = models.CharField('Name author', max_length=50)
+    comment_text = models.CharField('Comment text', max_length=200)
+
+    def __str__(self):
+        return self.author_name
